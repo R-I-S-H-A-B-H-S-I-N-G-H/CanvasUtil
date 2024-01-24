89ff353b6ccd1fa936133d3c208a903ab6acecc4
rishabh <rishabhsingh2305@gmail.com>
2024-01-24 22:42:02 +0530

    changes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		publicPath: "",
-		filename: "bundle.js",
+		filename: "canvasUtil.js",
 		library: "CanvasUtil",
 		libraryTarget: "umd",
 	},
